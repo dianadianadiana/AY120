c46b5d3e16a996462b9ec2fad16e2242edb9e0f4
Diana <Diana@Malgorzats-MBP.attlocal.net>
2015-10-22 23:34:53 -0700

    finalized the flat field correction

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 #==================================
 #Import Packages
 #==================================
@@ -11,7 +10,6 @@ import glob
 #import os #it tries to be IDL's SPAWN and falls short but it still useful
 import datetime as dt
 #==================================
-
 # How to use fits and astropy
 # http://docs.astropy.org/en/stable/io/fits/index.html
 #==================================
@@ -24,11 +22,11 @@ fits_list=glob.glob(datadir+datestr+'*.fits') #build list of images
 fig_path = '/Users/Diana/Desktop/Astro 120/Lab3/Figures/'
 #==================================
 
-#==========================
+#==================================
 # making arrays for the diff types of objects
 # each element of the array has the syntax of datapath + 'd100.fits'
 # DOES INCLUDE the actual file path
-#==========================
+#==================================
 bias_file_arr = [datapath + 'd'+str(i) + '.fits' for i in np.arange(100,115)]
 flatB_file_arr = [datapath + 'd'+str(i) + '.fits' for i in np.arange(115,118)]
 flatV_file_arr = [datapath + 'd'+str(i) + '.fits' for i in np.arange(118,121)]
@@ -38,59 +36,140 @@ WratR_file_arr = [datapath + 'd'+str(i) + '.fits' for i in np.arange(131,142)]
 WratI_file_arr = [datapath + 'd'+str(i) + '.fits' for i in np.arange(142,152)]
 DanaeR_file_arr = [datapath + 'd'+str(i) + '.fits' for i in np.arange(152,163)]
 DanaeI_file_arr = [datapath + 'd'+str(i) + '.fits' for i in np.arange(163,172)]
-#==========================
-#===========================
+#==================================
+#==================================
+#####  ======= WAYS THERE ARE ERRORS IN OUR FLAT FIELD CORRECTION
+#####  ======= TIPS/THOUGHTS TO KEEP IN MIND
+# (*) the bias is 0s exposure time -- can be used in any filter
+# (*) the flat is 5-10s exposure time and points at the sunset or sunrise -- when
+#     using this, make sure you have corresponding filters
+# (*) the last 32 cover pixels are there to take the bias if an actual bias img is
+#     not available; otherwise if a bias img is provided, the last 32 cover pixels
+#     are essentially not needed, so we can ignore them
+# (*) shape, (1024, 1056) == 1056 elements in each array; 1024 arrays
+#     on the xaxis = 1056, yaxis = 1024
+# (1) in the dead_pixels fn, we are taking the avg of the [1024,1024] img and not
+#     including the last 32 cover pixels. (I think it is safe to do so because the 
+#     actual data lies within [1024,1024]. 
+# (2) in the dead_pixels fn, we are setting arbitrary pixels to the average value;
+#     the pixels were chosen based on manual selection, and i chose to set them to 
+#     the avg and not 0 because i wanted a more "likely" value for the dead pixels
+# (3) in the loadfits fn, we return the img matrix corrected for the dead pixels and 
+#     has the dimensions of [1024,1024] (so we ignore/get rid of the cover pixels)
+# (4) in the avg_fits_img fn, we are trying to minimalize errors. <ex> When we have 14 bias 
+#     fits files, it is better to sum them all up and then take the average to get an
+#     averaged bias fits. Likewise, we can use this with the flat images. NOTE: the flat
+#     fits files have diff notes -- the diff notes may indicate that we can't just avg
+#     the flats, so what we do instead is noted in the next step.
+# (5) in the find_gain fn, we are finding the gain of each flat and then taking the avg
+#     gain. The reason we do this is due to the fact that the notes are diff for the flats.
+#     If the flats had different brightness levels / saturations [I think that's what
+#     those numbers, the 41k, etc. are?] it wouldn't skew the correction in any way.
+#     gain = np.mean(flat-bias)/(flat-bias) == this is a matrix
+# (6) the bias portion is just applying the functions to get the averaged bias
+# (7) the flats portion can be a little confusing, i will explain by sections
+#     (i)   i just loaded the img and hdr for each filter. only one file (instead of
+#           multiple) is used. this is not corrected at all except for the dead pixels
+#           and that i made them [1024,1024]
+#     (ii)  i then decided to avg all the flats using the same fn i used on the bias,
+#           they are not corrected in any way
+#     (iii) i then corrected the avg flats in (ii)
+#     (iv)  i thne approached the flat by doing the gain methed (see (5) for more info)
+# (8) Findings!
+#     I then plotted the differences in correcting the img by using the bias and flats
+#     I am very certain that finding the bias was a good way to go, I just wanted to see
+#     and test out how the different approaches in handling the flats would yield diff
+#     corrected displays. 
+#     honestly, i did not find THAT much of a difference, if any when using flat (i) (ii)
+#     or (iii). Maybe the error is not visible to us, but to the computer? Despite the fact
+#     that the displays look relatively the same, we decided to go with flat option (iv)
+#     because it really made little to no room for error for future calculations
+# (9) the correct_fits fn corrects the raw img, and by raw, i mean the img loaded in after
+#     the loadfits fn, so it is already dead pixel corrected and has the shape [1024,1024]
+#     the correction is: (img - bias) * gain
+#     and then we just display the beautiful graph!
+#==================================
+#==================================
 def dead_pixels(img):
-    avg = np.median(img)    
-    img[:,255]=avg; img[:,256]=avg; img[:,1002]=avg # dead pixels -- set to zero
+    """ There are many dead pixels in the CCD detector, so we just set them to avg
+    of the img when it is [1024,1024]"""
+    avg = np.median(img[:,:1024])    
+    img[:,255]=avg; img[:,256]=avg; img[:,1002]=avg # dead pixels -- set to avg
     img[:,1023]=avg; img[:,1024]=avg; img[:,1022]=avg 
     img[:,783]=avg; img[:,669]=avg
     return img
     
 def loadfits(fil):
-    """ Input: 
+    """ we take care of the dead pixels and we return the img W/O the cover pixels
+        Input: 
             fil: the filename, with the syntax of 'd100.fits'
         Output: 
-            img: the matrix of the fits file
+            img: the matrix of the fits file [1024,1024]
             hdr: the header of the fits file """
     hdulist = pf.open(fil)
     img = hdulist[0].data # the matrix data
     hdr= hdulist[0].header # the header
     print "***", np.median(img), '***'
     print hdr['object']
-    print img[:,256]
     img = dead_pixels(img)
     img = img[:,:1024] #ignore the 32 columns
     return [img, hdr]
     
-#==========================
-# average the bias arrays
-#===========================
-def avg_bias(bias_file_arr):
-    """ takes all the bias fits and makes them into one average bias fits array """
+def avg_fits_img(files_arr):
+    """ takes all the fits in the 'files_arr' and makes them into one average fits array (img) 
+        Note: no corrections are applied, just the raw fits files are used """
     #apparently this does the same thing, but I wanted to write it out myself
     #np.sum(bias_data_arr, axis = 0)/len(bias_data_arr)
-    bias_data_arr = [pf.getdata(bias_file_arr[i]) for i in range(len(bias_file_arr))]
-    bias_arr = bias_data_arr[0]
-    for i in range(1, len(bias_file_arr)):
-        bias_arr += bias_data_arr[i]
-    return bias_arr/(len(bias_file_arr))
-bias = avg_bias(bias_file_arr)
-bias = dead_pixels(bias)
-bias = bias[:,:1024]
-#===========================
+    data_arr = [pf.getdata(fil) for fil in files_arr]
+    avg_arr = data_arr[0]
+    for arr in data_arr[1:]:
+        avg_arr += arr
+    return avg_arr/(len(files_arr))
+    
+def find_gain(flat_files, bias):
+    def gain(x,y):
+        """ x: bias, y:flat -- both are the img matricies 
+            gain = np.mean(flat-bias)/(flat-bias) """
+        return np.mean(y-x)/(y-x)
+    data_arr = [dead_pixels(pf.getdata(fil))[:,:1024] for fil in flat_files] 
+    gain_arr = gain(bias, data_arr[0])
+    for flat in data_arr[1:]:
+        gain_arr += gain(bias, flat)
+    return gain_arr/(len(flat_files))
+#==================================
+#==================================
+# Bias 
+#==================================
+bias = avg_fits_img(bias_file_arr) # take all 14(?) bias files to create averaged one
+bias = dead_pixels(bias)[:,:1024]  # correct for dead pixels and then make it [1024,1024]
+#==================================
 
-#===========================
+#==================================
 # Flats
-#===========================
+#==================================
+# load the files normally (dead_pixels, and [:,:1024] are applied)
 flatB_img, flatB_hdr = loadfits(flatB_file_arr[0])
 flatV_img, flatV_hdr = loadfits(flatV_file_arr[0])
 flatR_img, flatR_hdr = loadfits(flatR_file_arr[0])
 flatI_img, flatI_hdr = loadfits(flatI_file_arr[0])
-#===========================
+# average the fits -- does not correct them in anyway [1024,1056]; NO correction for dead pixels
+flatB_img_avg = avg_fits_img(flatB_file_arr)
+flatV_img_avg = avg_fits_img(flatV_file_arr)
+flatR_img_avg = avg_fits_img(flatR_file_arr)
+flatI_img_avg = avg_fits_img(flatI_file_arr)
+# have to correct the avg fits [1024,1024]; YES correction for dead pixels
+flatB_img_avg = dead_pixels(flatB_img_avg)[:,:1024]
+flatV_img_avg = dead_pixels(flatV_img_avg)[:,:1024]
+flatR_img_avg = dead_pixels(flatR_img_avg)[:,:1024]
+flatI_img_avg = dead_pixels(flatI_img_avg)[:,:1024]
+# gain 
+gainB = find_gain(flatB_file_arr, bias)
+gainV = find_gain(flatV_file_arr, bias)
+gainR = find_gain(flatR_file_arr, bias)
+gainI = find_gain(flatI_file_arr, bias)
+#==================================
 
-#===========================
-    
+#==================================
 def display_info(fil):
     info = loadfits(fil)
     img, hdr = info[0], info[1]
@@ -116,36 +195,29 @@ display_info(WratR_file_arr[3])
 # header includes useful info like size of the image (NAXIS1 & NAXIS2), the data
 # (DATE-OBS), the exposure time (EXPTIME), where the celestial coordiantes of where 
 # telescopes was pointing (RA & DEC) and the optical filter in the light path (FILTNAM)
-    
-#shape, (1024, 1056) == 1056 elements in each array; 1024 arrays
-#on the xaxis = 1056, yaxis = 1024
-def bsub(bias, fil):
-    info = loadfits(fil)
-    img, hdr = info[0], info[1]
-    #cov = hdr['cover']
-    #average_bias= np.mean(bias[:,-cov:])
-    #img-=average_bias
-    img-=bias
-    return img
-def correct_fits(fil, bias, flat):
-    img, hdr = loadfits(fil)
-    return (img - bias)/(flat-bias) * (np.average(flat-bias))
-def display_fits(fil, flat_img):
+#==================================
+#==================================    
+def correct_fits(fil, bias, gain):
+    img, hdr = loadfits(fil) # img is corrected for deadpixels and [1024,1024]
+    return (img - bias) * gain
+def display_fits(fil, bias, gain):
     """Display fits using matplotlib"""    
     img, hdr = loadfits(fil)
-    img = correct_fits(fil, bias, flat_img)
+    img = correct_fits(fil, bias, gain)
     fig = plt.figure()
     plt.title(hdr['object'], fontsize=18)
     plt.imshow(img, origin='lower', interpolation='nearest', cmap='gray_r', 
                vmin=0.95*np.median(img), vmax=1.7*np.median(img))
     plt.colorbar()
     return fig
-    
-plt.show(display_fits(DanaeR_file_arr[5], flatR_img))
+
+plt.show(display_fits(DanaeR_file_arr[5], bias, gainR))
 #display_fits(DanaeR_file_arr[5], flatR_img).savefig(fig_path+'DanaeR_5_corrected.png',dpi=300)
-##============================
+#==================================
+#==================================
 
 
+#RANDOM -- from the lab handout
 #xb = bsub(bias, DanaeR_file_arr[5])
 #print np.mean(xb)
 #flatb = bsub(bias,flatR_file_arr[0])
@@ -166,11 +238,11 @@ plt.show(display_fits(DanaeR_file_arr[5], flatR_img))
 
 
 
+###IGNORE
 
-
-#============================
+#==================================
 # Centroids
-#============================
+#==================================
 def max_peaks(arr, width, lower_limit):
     """ Returns an array of the indexes where the indexes indicate where the peaks are"""
     #return [i for i in range(1, len(arr)-1) if arr[i-1]<arr[i] and arr[i+1]<arr[i] and arr[i]>lower_limit]
@@ -293,7 +365,11 @@ def centroids1(fil, flat_img):
         
     for index, elem in enumerate(img_arr):
         print index, elem
-    print len(img_arr)
+
+
+
+
+
 
     #for i_col in range(1, len(img)):
     #    print i_col, max_peaks(img[:,i_col], 10, lim)
@@ -376,7 +452,7 @@ def centroids1(fil, flat_img):
 #		#centroids.append(centroid)
 #	centroids, centroid_errors = np.transpose(centroids)[0], np.transpose(centroids)[1]
 #	return centroids, centroid_errors
-centroids1(DanaeR_file_arr[5], flatR_img)
+centroids1(DanaeR_file_arr[5], gainR)
 
     
 
